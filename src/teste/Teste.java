c6fa13320e570c372417fadef926e8a6808e2569
Ana Karolina Silva <86065008+anakarolinaaparecidasilva@users.noreply.github.com>
2022-06-08 22:38:47 -0300

    teste

---
+++ src/teste/Teste.java
@@ -0,0 +1,15 @@
+package teste;
+
+import model.Gato;
+
+public class Teste {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        int a=5;
+        int b=3;
+        System.out.println("Olá Mundo!" + (a+b));
+    }
+
+}
