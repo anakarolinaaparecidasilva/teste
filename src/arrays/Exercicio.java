832de5f5ec57d4073874f45e39de537aa14ca2a4
Ana Karolina Silva <86065008+anakarolinaaparecidasilva@users.noreply.github.com>
2022-06-09 00:44:04 -0300

    array multidimensional

---
+++ src/arrays/Exercicio.java
@@ -0,0 +1,22 @@
+package arrays;
+
+
+public class Exercicio {
+    public static void main(String[] args) {
+
+       // Forma de definir um array com 10 posições, onde as 2 primeiras são dadas e as demais não.
+        int[] idades = new int [10];
+        idades [0] = 27;
+        idades [1] = 31;
+        System.out.println("Idade 0: " + idades [0]);
+        System.out.println("Idade 1: " + idades [1]);
+        System.out.println("Idade 2: " + idades [2]);
+
+        // nesse os elementos do array já sao definidos:
+
+        int [] i = {10,25};
+        System.out.println("i 0: " + idades [0]);
+        System.out.println("i 1: " + idades [1]);
+
+    }
+}
